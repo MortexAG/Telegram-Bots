ad5638881ed728e8b894d9146411d4537f17900f
MortexAG <107272049+MortexAG@users.noreply.github.com>
2022-08-12 16:15:24 +0200

    Changed The API
    Now Using A Completely Free API from ("https://open-meteo.com/en") and Removed The Functionality To Tell The Weather Condition As It's Not Present in This API.

--- Weather Bot/main.py
+++ Weather Bot/main.py
@@ -5,13 +5,6 @@ import keep_alive
 
 BOT_API = os.environ['BOT_KEY']
 
-WEATHER_API = os.environ['WEATHER_API']
-
-headers = {
-    'x-api-key': WEATHER_API,
-    'Content-type': "application/json"
-    }
-
 bot = telebot.TeleBot(BOT_API)
 @bot.message_handler(commands=['start', 'help'])
 # The Help And Start Commands
@@ -20,52 +13,52 @@ def help(message):
 # Get Mecca Weather Command
 @bot.message_handler(commands=['mecca'])
 def mecca(message):
-  lat = 21.455841422340242
-  lng = 39.87942219775681
-  url = "https://api.ambeedata.com/weather/latest/by-lat-lng?lat={}&lng={}".format(lat, lng)
-  response = requests.get(url, headers=headers)
-  data = response.json()
-  f = data['data']['temperature']
-  celsius = (f-32)*(5/9)
-  c = round(celsius)
-  weather = data['data']['summary']
-  bot.send_message(message.chat.id, "Tempreture in Mecca now is Around ({} C°)".format(c))
-  bot.send_message(message.chat.id, "The Weather is ({})".format(weather))
+  try:
+    lat = 21.455841422340242
+    lng = 39.87942219775681
+    url = "https://api.open-meteo.com/v1/forecast?latitude={}&longitude={}&current_weather=true".format(lat, lng)
+    response = requests.get(url)
+    data = response.json()
+    f = data['current_weather']['temperature']
+    c = round(f)
+    bot.send_message(message.chat.id, "Tempreture in Mecca now is Around ({} C°)".format(c))
+  except:
+    bot.send_message(message.chat.id, "Sorry The Bot is Not Working Now, Try Later or Send To The Maker For Debugging")
 
 # Get Cairo Weather Command
 @bot.message_handler(commands=['cairo'])
 def cairo(message):
-  lat = 30.046321259557125
-  lng = 31.244240957699272
-  url = "https://api.ambeedata.com/weather/latest/by-lat-lng?lat={}&lng={}".format(lat, lng)
-  response = requests.get(url, headers=headers)
-  data = response.json()
-  f = data['data']['temperature']
-  c = (f-32)*(5/9)
-  celsius = round(c)
-  weather = data['data']['summary']
-  bot.send_message(message.chat.id, "Tempreture in Cairo now is Around ({} C°)".format(celsius))
-  bot.send_message(message.chat.id, "The Weather is ({})".format(weather))
-
+  try:
+    lat = 30.046321259557125
+    lng = 31.244240957699272
+    url = "https://api.open-meteo.com/v1/forecast?latitude={}&longitude={}&current_weather=true".format(lat, lng)
+    response = requests.get(url)
+    data = response.json()
+    f = data['current_weather']['temperature']
+    celsius = round(f)
+    bot.send_message(message.chat.id, "Tempreture in Cairo now is Around ({} C°)".format(celsius))
+  except:
+    bot.send_message(message.chat.id, "Sorry The Bot is Not Working Now, Try Later or Send To The Maker For Debugging")
 @bot.message_handler(commands=['send_location'])
 def coordinates(message):
   bot.send_message(message.chat.id, "Send The Location Please")
   @bot.message_handler(func=lambda message: True,       content_types='location')
   def location(message):
-    message = message
-    # extract the exact location from the location sent
-    lat = message.location.latitude
-    lng = message.location.longitude
-    # run the api as usual
-    url = "https://api.ambeedata.com/weather/latest/by-lat-lng?lat={}&lng={}".format(lat, lng)
-    response = requests.get(url, headers=headers)
-    data = response.json()
-    f = data['data']['temperature']
-    c = (f-32)*(5/9)
-    celsius = round(c)
-    weather = data['data']['summary']
-    bot.send_message(message.chat.id, "Tempreture in This Location now is Around ({} C°)".format(celsius))
-    bot.send_message(message.chat.id, "The Weather in This Location is ({})".format(weather))
+    try:
+      message = message
+      # extract the exact location from the location sent
+      lat = message.location.latitude
+      lng = message.location.longitude
+      # run the api as usual
+      url = "https://api.open-meteo.com/v1/forecast?latitude={}&longitude={}&current_weather=true".format(lat, lng)
+      response = requests.get(url)
+      data = response.json()
+      f = data['current_weather']['temperature']
+      celsius = round(f)
+      bot.send_message(message.chat.id, "Tempreture in This Location now is Around ({} C°)".format(celsius))
+
+    except:
+      bot.send_message(message.chat.id, "Sorry The Bot is Not Working Now, Try Later or Send To The Maker For Debugging")
 
 
 bot.infinity_polling()
