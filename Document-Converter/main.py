e686f506d70b7297c7fc1d8407b9ee997e659514
MortexAG <107272049+MortexAG@users.noreply.github.com>
2023-07-10 13:33:33 +0300

    Image Conversion To PDF
    The Bot Can Now Convert Any Image Sent Directly In The Chat To A Pdf Document, Also I Added Checks For The Correct Directories So They are Created Automatically When The Bot Runs

--- Document-Converter/main.py
+++ Document-Converter/main.py
@@ -2,10 +2,39 @@ import os
 import telebot
 import convertapi
 import glob
+import PIL
+from PIL import Image
 import keep_alive
 BOT_API = os.environ["BOT_API"]
 API_SECRET = os.environ['API_SECRET']
 bot = telebot.TeleBot(BOT_API)
+
+# Path Checks For The Important Directories
+
+if os.path.exists("./converted"):
+  pass
+else:
+  os.mkdir("./downloaded")
+  
+if os.path.exists("./downloaded"):
+  pass
+else:
+  os.mkdir("./downloaded")
+
+if os.path.exists("./result"):
+  pass
+else:
+  os.mkdir("./result")
+  
+if os.path.exists("./images"):
+  pass
+else:
+  os.mkdir("./images")
+if os.path.exists("./images/photos"):
+  pass
+else:
+  os.mkdir("./images/photos")
+
 def empty_converted():
   for filename in glob.glob('converted/*'):
     file_converted_delete = filename
@@ -14,6 +43,20 @@ def empty_downloaded():
   for filename in glob.glob('downloaded/*'):
     file_downloaded_delete = filename
     os.remove(file_downloaded_delete)
+
+def empty_images():
+  for filename in glob.glob('images/photos/*'):
+    file_downloaded_delete = filename
+    os.remove(file_downloaded_delete)
+
+def empty_result():
+  for filename in glob.glob('result/*'):
+    file_downloaded_delete = filename
+    os.remove(file_downloaded_delete)
+
+
+empty_images()
+empty_result()
 
 empty_converted()
 empty_downloaded()
@@ -50,6 +93,24 @@ def powerpoint_to_pdf():
 
 def get_downloaded_file():
   bot.get_file()
+
+def one_image_to_pdf():
+
+  names = 0
+  for i in glob.glob("./images/photos/*"):
+
+    #Open The Each Image
+    image_open = Image.open(i)
+
+    #Convert Each One To RGB
+    rgb_image = image_open.convert("RGB")
+
+    #Naming Order For Pdf
+    names += 1
+
+    #Convert each image to pdf
+    rgb_image.save(f"./result/result.pdf")
+    empty_images()
 
 @bot.message_handler(commands=["start", "keyboard"])
 def start_message(message):
@@ -113,6 +174,25 @@ def document(message):
         bot.send_message(message.chat.id, "Please Send PDF or Word Documents Only")
         empty_converted()
         empty_downloaded()
+
+@bot.message_handler(content_types=['photo'])
+def image_convert(message):
+    bot.reply_to(message, "Image Recieved, Converting To PDF")
+
+    fileID = message.photo[-1].file_id
+    #print ('fileID =', fileID)
+    file_info = bot.get_file(fileID)
+    file_new_name = file_info.file_path.replace("./images/", "")
+    #print ('file.file_path =', file_info.file_path)
+    downloaded_file = bot.download_file(file_info.file_path)
+    with open(f"./images/{file_new_name}", 'wb') as new_file:
+      new_file.write(downloaded_file)
+    one_image_to_pdf()
+    final_pdf = open("./result/result.pdf", "rb")
+    bot.send_message(message.from_user.id, "Image Converted To Pdf")
+    bot.send_document(message.from_user.id, final_pdf)
+    empty_images()
+    empty_result()
 
 
 bot.infinity_polling()
